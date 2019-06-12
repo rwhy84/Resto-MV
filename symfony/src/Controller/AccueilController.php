b0a2eb211e6cb61fe22cb9ff3cd450c4410e3308
rwhy84 <rbordet84@gmail.com>
2019-06-12 15:38:13 +0200

    Injection formulaire newsletter sur toutes les pages
    Controller mis a jour sur chaque pages publique.

--- symfony/src/Controller/AccueilController.php
+++ symfony/src/Controller/AccueilController.php
@@ -4,16 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
 
 class AccueilController extends AbstractController
 {
     /**
      * @Route("/accueil", name="accueil")
      */
-    public function index()
+    public function index(Request $request): Response
     {
+
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('accueil_index');
+        }
+
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'contact' => $newsletter,
+            'form' => $form->createView(),
         ]);
     }
 }
--- symfony/src/Controller/ContactController.php
+++ symfony/src/Controller/ContactController.php
@@ -4,16 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request): Response
     {
+
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('galerie_index');
+        }
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'contact' => $newsletter,
+            'form' => $form->createView(),
         ]);
     }
 }
