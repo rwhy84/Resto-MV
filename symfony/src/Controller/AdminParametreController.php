379e1ac481218d8bf5e3a6507ccf59002b771027
rwhy84 <48202702+rwhy84@users.noreply.github.com>
2019-06-17 22:34:35 +0200

    Creneau Read -> Parametre
    Read des créneaux sur la page parametre en type hidden

--- symfony/src/Controller/AdminParametreController.php
+++ symfony/src/Controller/AdminParametreController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CreneauRepository;
 
 class AdminParametreController extends AbstractController
 {
     /**
      * @Route("/admin/parametre", name="admin_parametre")
      */
-    public function index()
+    public function index(Request $request, CreneauRepository $creneauRepository): Response
     {
         return $this->render('admin_parametre/index.html.twig', [
             'controller_name' => 'AdminParametreController',
+            'creneaux' => $creneauRepository->findBy([], ["id" => "ASC"]),
         ]);
     }
 }
