844d7cbcab445d2a9d423a35531103d77fdc534a
mozart0919 <moiseguadalupe@hotmail.com>
2019-06-17 17:32:26 +0200

    fddfdfdf

--- symfony/src/Migrations/Version20190617121025.php
+++ symfony/src/Migrations/Version20190617121025.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190617083919 extends AbstractMigration
+final class Version20190617121025 extends AbstractMigration
 {
     public function getDescription() : string
     {
